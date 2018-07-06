0e4167b0b78a032139501330fb944f76ef2bf963
nevakee716 <nevakee716@gmail.com>
2018-07-06 12:25:28 +0200

    allow to map color to value

--- src/cwChartCustomColor.js
+++ src/cwChartCustomColor.js
@@ -9,25 +9,30 @@
     cwCustomerSiteActions.cwChartCustomColors.ColorByView = {};
 
     // custom colors for specifics views
-    cwCustomerSiteActions.cwChartCustomColors.ColorByView.index_process_lifecycle_deliver = ['#0A5294','#093395','#091397','#200899','#42089B','#65089D','#89079F','#A10795','#A30773','#A5064F','#A7062B','#A90505','#AB2B05','#AD5204','#AF7A04','#B1A303','#98B303','#71B502','#48B702','#1FB901','#01BB0D','#00BD38','#00BF64'];
-   
+    cwCustomerSiteActions.cwChartCustomColors.ColorByView.index_process_lifecycle_deliver = {};
+    cwCustomerSiteActions.cwChartCustomColors.ColorByView.index_process_lifecycle_deliver.colors = ['#0A5294','#093395','#091397','#200899','#42089B','#65089D','#89079F','#A10795','#A30773','#A5064F','#A7062B','#A90505','#AB2B05','#AD5204','#AF7A04','#B1A303','#98B303','#71B502','#48B702','#1FB901','#01BB0D','#00BD38','#00BF64'];
+    cwCustomerSiteActions.cwChartCustomColors.ColorByView.index_process_lifecycle_deliver.mapping = {Tâche : '#EEEEEE'};
     // default color 
-    cwCustomerSiteActions.cwChartCustomColors.ColorByView.default = ['#000000', '#999999', '#FFFFFF', '#d84a28', '#93341f', '#c3c9ca', '#dd5b26', '#1b5894', '#e68d45', '#93351f', '#ba3d25', '#67b0d6', '#d74927', '#e58941', '#d84a28', '#67b0d6', '#ba3d25', '#93341f', '#e68d44', '#e68d44', '#dd5b26', '#c4c9ca', '#59a2cc'];
-
+    cwCustomerSiteActions.cwChartCustomColors.ColorByView.default = {};
+    cwCustomerSiteActions.cwChartCustomColors.ColorByView.default.colors = ['#000000', '#999999', '#FFFFFF', '#d84a28', '#93341f', '#c3c9ca', '#dd5b26', '#1b5894', '#e68d45', '#93351f', '#ba3d25', '#67b0d6', '#d74927', '#e58941', '#d84a28', '#67b0d6', '#ba3d25', '#93341f', '#e68d44', '#e68d44', '#dd5b26', '#c4c9ca', '#59a2cc'];
+    cwCustomerSiteActions.cwChartCustomColors.ColorByView.default.mapping = {Tâche : '#EEEEEE'};
 
 
     /********************************************************************************
     Custom Action for Single Page : See Impact here http://bit.ly/2qy5bvB
     *********************************************************************************/
     cwCustomerSiteActions.doActionsForSingle_Custom = function (rootNode) { 
-        var currentView, url,i,cwView;
+        var currentView, url,i;
         currentView = cwAPI.getCurrentView();
-        if(currentView) cwView = currentView.cwView;
 
         for(i in cwAPI.customLibs.doActionForSingle) {
             if(cwAPI.customLibs.doActionForSingle.hasOwnProperty(i)) {
                 if (typeof(cwAPI.customLibs.doActionForSingle[i]) === "function"){
-                    cwAPI.customLibs.doActionForSingle[i](rootNode,cwView);
+                    if(currentView) {
+                        cwAPI.customLibs.doActionForSingle[i](rootNode,currentView.cwView);
+                    } else {
+                        cwAPI.customLibs.doActionForSingle[i](rootNode,null);
+                    }
                 }   
             }
         }
@@ -38,14 +43,13 @@
     Custom Action for Index Page : See Impact here http://bit.ly/2qy5bvB
     *********************************************************************************/
     cwCustomerSiteActions.doActionsForIndex_Custom = function (rootNode) { 
-        var currentView, url,i,cwView;
+        var currentView, url,i;
         currentView = cwAPI.getCurrentView();
-        if(currentView) cwView = currentView.cwView;
 
         for(i in cwAPI.customLibs.doActionForIndex) {
             if(cwAPI.customLibs.doActionForIndex.hasOwnProperty(i)) {
                 if (typeof(cwAPI.customLibs.doActionForIndex[i]) === "function"){
-                    cwAPI.customLibs.doActionForIndex[i](rootNode,cwView);
+                    cwAPI.customLibs.doActionForIndex[i](rootNode,currentView.cwView);
                 }   
             }
         }
@@ -53,13 +57,62 @@
 
     cwCustomerSiteActions.cwChartCustomColors.changeColors = function(rootNode,cwView){
         if(cwCustomerSiteActions.cwChartCustomColors.ColorByView.hasOwnProperty(cwView) ) {
-            cwApi.cwReporting.colorRange = cwCustomerSiteActions.cwChartCustomColors.ColorByView[cwView];
+            cwApi.cwReporting.colorRange = cwCustomerSiteActions.cwChartCustomColors.ColorByView[cwView].colors;
         } else {
-            cwApi.cwReporting.colorRange = cwCustomerSiteActions.cwChartCustomColors.ColorByView.default;
+            cwApi.cwReporting.colorRange = cwCustomerSiteActions.cwChartCustomColors.ColorByView.default.colors;
         }
         
     };
 
+
+
+
+    cwApi.CwCharts.CwChart.prototype.updateCommonOptionsProperties = function(options) {
+        var mapping,colors,cwView = cwAPI.getCurrentView();
+        var self = this;
+        if(cwView && cwView.cwView && cwCustomerSiteActions.cwChartCustomColors.ColorByView.hasOwnProperty(cwView) ) {
+            mapping = cwCustomerSiteActions.cwChartCustomColors.ColorByView[cwView].mapping;
+            colors = cwCustomerSiteActions.cwChartCustomColors.ColorByView[cwView].colors;
+        } else {
+            mapping = cwCustomerSiteActions.cwChartCustomColors.ColorByView.default.mapping;
+            colors = cwCustomerSiteActions.cwChartCustomColors.ColorByView.default.colors;
+        }
+
+        if(options && options.series) {
+            options.series.forEach(function(s) {
+                s.data.forEach(function(d,i) {
+                    if(mapping.hasOwnProperty(d.category)) {
+                        cwApi.cwReporting.colorRange[i] = mapping[d.category];
+                    } 
+                });
+            });
+        };
+
+
+
+
+        options.legend = {};
+        if (this.title !== null) {
+            options.title = {
+                text: this.title,
+                font: 'large inherits'
+            };
+        }
+        options.theme = 'metro';
+        options.width = '100%';
+        options.tooltip = {
+            visible: true,
+            font: 'inherit',
+            opacity: 1
+        };
+        if (!cwApi.isUndefined(this.seriesClick)) {
+            options.seriesClick = this.seriesClick;
+        }
+        options.seriesColors = cwAPI.cwReporting.colorRange;
+        options.chartArea = {
+            background: "transparent"
+        };
+    };
 
     /********************************************************************************
     Configs : add trigger for single and index
